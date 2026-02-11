37e2a008a115d9108d2bd4148dc78aa5404869b6
pradeek <pradeek.k@gmail.com>
2026-02-11 12:12:36 +0530

    09 completed

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -53,6 +53,7 @@ func (store Store) TransferTx(ctx context.Context, arg TransferTxParams) (Transf
 	var result TransferTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
+
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
 			ToAccountID:   arg.ToAccountID,
@@ -61,6 +62,7 @@ func (store Store) TransferTx(ctx context.Context, arg TransferTxParams) (Transf
 		if err != nil {
 			return err
 		}
+
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
 			Amount:    -arg.Amount,
@@ -68,6 +70,7 @@ func (store Store) TransferTx(ctx context.Context, arg TransferTxParams) (Transf
 		if err != nil {
 			return err
 		}
+
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
@@ -75,29 +78,24 @@ func (store Store) TransferTx(ctx context.Context, arg TransferTxParams) (Transf
 		if err != nil {
 			return err
 		}
-		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
-		if err != nil {
-			return err
-		}
-		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
 			ID:      arg.FromAccountID,
-			Balance: account1.Balance - arg.Amount,
+			Amount: -arg.Amount,
 		})
 		if err != nil {
 			return err
 		}
 
-		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
-		if err != nil {
-			return err
-		}
-		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
 			ID:      arg.ToAccountID,
-			Balance: account2.Balance + arg.Amount,
+			Amount: arg.Amount,
 		})
 		if err != nil {
 			return err
 		}
+
 		return nil
 	})
 
